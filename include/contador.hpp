bd66123c14233297b613e7fb38298ba8d5f8b1da
guilodron <guilodron7@gmail.com>
2018-10-11 10:08:16 -0300

    definidas as funcoes a serem implementadas no arquivo header

---
+++ include/contador.hpp
@@ -0,0 +1,10 @@
+// "Copyright [2018] <Guilherme Rodrigues Lodron Pires>"
+#ifndef __CONTADOR_HPP__
+#define __CONTADOR_HPP__
+
+int total_linhas(FILE* ptr);
+int linhas_em_branco(FILE* ptr);
+int linhas_comentadas(FILE* ptr);
+int num_linhas(FILE* ptr);
+
+#endif
